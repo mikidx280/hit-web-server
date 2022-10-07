1aa7c0d0860330deb1d942fe6eed0be119a568fc
Michael Rahmanizad <99471722+mikidx280@users.noreply.github.com>
2022-10-07 12:14:01 +0300

    Update webApplicationTests.java

--- automation/src/test/java/webApplicationTests.java
+++ automation/src/test/java/webApplicationTests.java
@@ -1,9 +1,12 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.junit.jupiter.api.Assertions;
+
 
 import static io.restassured.RestAssured.when;
 
@@ -18,14 +21,24 @@ public class webApplicationTests {
 
     @Test
     public void testUI() {
+
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\shay\\Desktop\\auotoCAD\\chromedriver.exe");
         driver = new ChromeDriver();
+
         String domain = "http://localhost:2020/";
+        Response response=RestAssured.get(domain);
+
         String nameAndIdStudent = "Mahyar Rahmaniezad - 324525237";
         String realNameIdStudent;
         driver.get(domain);
         realNameIdStudent = driver.findElement(By.id("Mahyar Rahmaniezad")).getText();
+
+        //rest assured 5.a
+        Assertions.assertEquals(200,response.getStatusCode());
+
+        //selenium 5.b
         Assertions.assertEquals(nameAndIdStudent, realNameIdStudent);
+
 
 
 
